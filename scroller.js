26d2b302002fd0c694da86ff2376b53012d02b1d
Pat Cavit <pcavit@gmail.com>
2014-08-21 14:59:40 -0700

    Grabbing!
    Needs to be clamped, however.

--- scroller.js
+++ scroller.js
@@ -3,19 +3,42 @@
         if(!config) {
             config = {};
         }
-
+        
         this._outer  = el;
         this._inner  = el.querySelector(".inner");
-        this._handle = el.querySelector(".scrollbar .handle");
+        this._scroll = el.querySelector(".scrollbar");
+        this._handle = this._scroll.querySelector(".handle");
         
         if(!this._inner && !this._handle) {
             throw new Error("Missing .inner or .handle elements");
         }
+        
+        this._events  = [];
 
         this.attach();
     }
     
     Scroller.prototype = {
+        // DOM event binding niceities
+        _on : function(el, ev, fn) {
+            el.addEventListener(ev, fn);
+            
+            return this._events.push({
+                el : el,
+                ev : ev,
+                fn : fn
+            });
+        },
+        
+        _off : function(id) {
+            var events = id ? this._events.splice(id, 1) : this._events;
+            
+            events.forEach(function(e) {
+                e.el.removeEventListener(e.ev, e.fn);
+            });
+        },
+        
+        // Public-ish API
         attach : function() {
             var observer = new MutationObserver(throttler(this._calc.bind(this)));
 
@@ -24,39 +47,79 @@
                 subtree   : true
             });
             
-            this._inner.addEventListener("scroll", throttler(this._onScroll.bind(this)));
+            this._on(this._inner, "scroll", throttler(this._onScroll.bind(this)));
+            this._on(this._handle, "mousedown", this._onGrab.bind(this));
             
             this._calc();
         },
         
         detach : function() {
-            // TODO: someday
+            this._off();
         },
         
+        // Utility Methods
         _calc : function() {
-            console.log("_calc");
+            var heights;
             
-            var heights  = {
-                    outer  : this._outer.getBoundingClientRect().height,
-                    inner  : this._inner.scrollHeight,
-                    handle : this._handle.getBoundingClientRect().height
-                };
+            this._rect    = this._outer.getBoundingClientRect();
+            this._heights = heights = {
+                outer  : this._rect.height,
+                inner  : this._inner.scrollHeight,
+                handle : this._handle.getBoundingClientRect().height
+            };
             
-            this._ratio = (heights.outer / heights.inner);
+            console.log(this._rect);
             
-            console.log(heights, this._ratio);
+            this._stepDown = (heights.outer / heights.inner);
+            this._stepUp   = (heights.inner / heights.outer);
             
-            this._heights = heights;
+            console.log(this._stepDown, this._stepUp);
         },
         
+        // Event handlers
+        // TODO: this never scrolls ALL the way to the bottom
         _onScroll : function() {
-            console.log("scroll", this._inner.scrollTop);
-            
-            var pos = Math.round(this._inner.scrollTop * this._ratio);
-            
-            console.log(pos);
+            var pos = Math.round(this._inner.scrollTop * this._stepDown);
             
             this._handle.style.transform = "translateY(" + pos + "px)";
+        },
+        
+        _onGrab : function(e) {
+            console.log("grabbed", e);
+            
+            if(e.which !== 1) {
+                return;
+            }
+            
+            e.preventDefault();
+            
+            this._dragging = [
+                this._on(document, "mousemove", throttler(this._onMove.bind(this))),
+                this._on(document, "mouseup", this._onRelease.bind(this))
+            ];
+        },
+        
+        _onMove : function(e) {
+            var offset;
+            
+            if(!this._dragging) {
+                return;
+            }
+            
+            offset = e.pageY - this._rect.top;
+            
+            // Keep handle in sync
+            this._handle.style.transform = "translateY(" + offset + "px)";
+            
+            // Keep inner in sync
+            this._inner.scrollTop = Math.round(offset * this._stepUp);
+        },
+        
+        _onRelease : function(e) {
+            console.log("released");
+            
+            this._dragging.forEach(this._off.bind(this));
+            this._dragging = false;
         }
     };
 
